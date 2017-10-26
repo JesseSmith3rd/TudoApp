f0d54c8642280641d8bb72e34d13480e5ff29e24
Jesse Smith <JesseSmith3rd@outlook.com>
2017-10-25 21:21:51 -0400

    Add array, add constructor, add super, add this.state to access tudos

--- tudoapp/src/App.js
+++ tudoapp/src/App.js
@@ -2,18 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Let's create an TudoArray
+var todos = [
+  {
+    todoTitle: "My first todo",
+    todoResponsible: "Jesse",
+    todoDescription: "My first todo description",
+    todoPriority: "low"
+  },
+  {
+    todoTitle: "My second todo",
+    todoResponsible: "Roshonda",
+    todoDescription: "My second todo description",
+    todoPriority: "medium"
+  },
+  {
+    todoTitle: "My third todo",
+    todoResponsible: "Laila",
+    todoDescription: "My third todo description",
+    todoPriority: "high"
+  }
+]
+
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      todos
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React Jesse !!!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      
     );
   }
 }
